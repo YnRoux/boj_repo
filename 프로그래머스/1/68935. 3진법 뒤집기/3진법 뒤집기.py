95ec2567c4361c3042ca0599b9d4f31c442478d4
Peter Ryu <101504238+YnRoux@users.noreply.github.com>
2026-01-07 21:42:11 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.05 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -6,5 +6,6 @@ def solution(n):
     for i in range(idx_max, -1, -1):
         digits.append(n//3**i)
         n %= 3 ** i
-    val = sum([digits[-i] * 3 ** (idx_max+1 - i) for i in range(1, idx_max+2)])
+    digits.reverse()
+    val = sum([digits[i] * 3 ** (idx_max - i) for i in range(idx_max +1)])
     return val
