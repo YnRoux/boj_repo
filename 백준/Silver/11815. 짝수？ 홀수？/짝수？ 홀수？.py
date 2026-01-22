4c4a9fdb309142c4f4fcc0319c34f4a1809e9814
Peter Ryu <101504238+YnRoux@users.noreply.github.com>
2026-01-22 12:54:40 +0900

    [Silver IV] Title: 짝수? 홀수?, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11815. 짝수？ 홀수？/짝수？ 홀수？.py
@@ -0,0 +1,8 @@
+n = int(input())
+num = list(map(int, input().split()))
+
+for i in range(n):
+    if num[i] == (int(num[i] ** 0.5) ** 2):
+        print(1, end = " ")
+    else:
+        print(0, end = " ")
