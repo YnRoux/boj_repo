b56c3b2a0c002558ad0d9c51c76d2b7035099205
Peter Ryu <101504238+YnRoux@users.noreply.github.com>
2026-01-28 14:00:04 +0900

    [Silver III] Title: 2×n 타일링, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,10 +1,12 @@
 n = int(input())
 
-dp = [0] * 1001
-dp[1] = 1
-dp[2] = 2
+a, b = 1, 2
 
-for i in range(3, n+1):
-    dp[i] = (dp[i-1] + dp[i-2]) % 10007
-
-print(dp[n])
+if n == 1:
+    print(1)
+elif n == 2:
+    print(2)
+else:
+    for _ in range(3, n + 1):
+        a, b = b, (a + b) % 10007
+    print(b)
