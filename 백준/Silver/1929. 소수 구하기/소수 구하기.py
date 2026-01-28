3043b034cdeb669c16564d5ed3d86dd65ba419fa
Peter Ryu <101504238+YnRoux@users.noreply.github.com>
2026-01-28 14:36:49 +0900

    [Silver III] Title: 소수 구하기, Time: 236 ms, Memory: 44076 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,8 +1,12 @@
 m, n = map(int, input().split())
-for i in range(m, n+1):
-    if i == 1: continue
-    if i == 2: print(2)
-    if i % 2 == 0: continue
-    for j in range(3, int(i**(1/2))+1, 2):
-        if i % j == 0: break
-    else: print(i)
+
+is_prime = [True] * (n + 1)
+is_prime[1] = False
+for i in range(2, int(n**0.5) + 1):
+    if is_prime[i]:
+        for j in range(i*i, n + 1, i):
+            is_prime[j] = False
+
+results = [str(i) for i in range(m, n + 1) if is_prime[i]]
+
+print('\n'.join(results))
