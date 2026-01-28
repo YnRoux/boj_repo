c13d5ffc3dfec51dbb6760483aac9f4173a8bb7c
Peter Ryu <101504238+YnRoux@users.noreply.github.com>
2026-01-28 15:21:43 +0900

    [Gold V] Title: 적록색약, Time: 68 ms, Memory: 34992 KB -BaekjoonHub

---
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -0,0 +1,46 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+n = int(input())
+grid = [list(input().rstrip()) for _ in range(n)]
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(x, y, color, visited, current_grid):
+    queue = deque([(x, y)])
+    visited[x][y] = True
+
+    while queue:
+        cx, cy = queue.popleft()
+
+        for i in range(4):
+            nx, ny = cx + dx[i], cy + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < n:
+                if not visited[nx][ny] and current_grid[nx][ny] == color:
+                    visited[nx][ny] = True
+                    queue.append((nx, ny))
+
+def get_area_count(current_grid):
+    visited = [[False] * n for _ in range(n)]
+    count = 0
+
+    for i in range(n):
+        for j in range(n):
+            if not visited[i][j]:
+                bfs(i, j, current_grid[i][j], visited, current_grid)
+                count += 1
+    return count
+
+normal_count = get_area_count(grid)
+
+for i in range(n):
+    for j in range(n):
+        if grid[i][j] == 'G':
+            grid[i][j] = 'R'
+
+blind_count = get_area_count(grid)
+
+print(f"{normal_count} {blind_count}")
