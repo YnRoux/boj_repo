37f7e10342aeec7f9c0c266e0bb706282a64d40b
Peter Ryu <101504238+YnRoux@users.noreply.github.com>
2026-01-29 13:50:09 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.cc
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -4,20 +4,6 @@ using namespace std;
 int main(int argc, char const *argv[]) {
     int y;
     cin >> y;
-    
-    if (y % 4 == 0) {
-        if (y % 400 == 0) {
-            cout << 1;
-        }
-        else if (y % 100 != 0) {
-            cout << 1;
-        }
-        else {
-            cout << 0;
-        }
-    }
-    else {
-        cout << 0;
-    }
+    cout << ((y % 4 == 0) ? ((y % 400 == 0) ? 1 : (y % 100 != 0) ? 1 : 0) : 0);
     return 0;
 }
