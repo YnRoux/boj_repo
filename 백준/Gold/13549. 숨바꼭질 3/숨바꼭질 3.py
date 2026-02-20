d63e8d1bb20fe9a2aaf1a0b10411e865c31c87ba
Peter Ryu <101504238+YnRoux@users.noreply.github.com>
2026-02-20 22:54:01 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 96 ms, Memory: 37624 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,34 +1,29 @@
 import sys
-import heapq
+from collections import deque
 
 def solve():
     n, k = map(int, sys.stdin.readline().split())
     
-    if n >= k:
-        print(n - k)
-        return
-
     MAX = 100001
-    dist = [float('inf')] * MAX
+    dist = [-1] * MAX
     
+    queue = deque([n])
     dist[n] = 0
-    pq = [(0, n)]
     
-    while pq:
-        d, curr = heapq.heappop(pq)
-        
-        if dist[curr] < d:
-            continue
+    while queue:
+        curr = queue.popleft()
         
         if curr == k:
-            print(d)
+            print(dist[curr])
             return
 
-        for next_step, weight in [(curr * 2, 0), (curr - 1, 1), (curr + 1, 1)]:
-            next_dist = d + weight
-            
-            if 0 <= next_step < MAX and next_dist < dist[next_step]:
-                dist[next_step] = next_dist
-                heapq.heappush(pq, (next_dist, next_step))
+        for next_pos in [curr * 2, curr - 1, curr + 1]:
+            if 0 <= next_pos < MAX and dist[next_pos] == -1:
+                if next_pos == curr * 2:
+                    dist[next_pos] = dist[curr]
+                    queue.appendleft(next_pos)
+                else:
+                    dist[next_pos] = dist[curr] + 1
+                    queue.append(next_pos)
 
 solve()
