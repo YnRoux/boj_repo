f89f96411dd985031450177dc1e4067bd2c489e7
Peter Ryu <101504238+YnRoux@users.noreply.github.com>
2026-03-01 20:21:33 +0900

    [Silver III] Title: Four Squares, Time: 120 ms, Memory: 111324 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -0,0 +1,12 @@
+def solve():
+    n = int(input())
+    dp = [0] + [4] * n
+
+    for i in range(1, n+1):
+        for j in range(1, int(i**(0.5))+1):
+            dp[i] = min(dp[i], dp[i - j**2] + 1)
+            
+    print(dp[-1])
+
+if __name__ == "__main__":
+    solve()
