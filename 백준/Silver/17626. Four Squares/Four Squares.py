d5b5d1e556c7f5d0a3548d516e5098c0ca529dc4
Peter Ryu <101504238+YnRoux@users.noreply.github.com>
2026-03-01 20:41:11 +0900

    [Silver III] Title: Four Squares, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -1,12 +1,28 @@
+def is_square(n):
+    if n < 0:
+        return False
+    root = int(n**0.5)
+    return root**2 == n
+
 def solve():
     n = int(input())
-    dp = [0] + [4] * n
 
-    for i in range(1, n+1):
-        for j in range(1, int(i**(0.5))+1):
-            dp[i] = min(dp[i], dp[i - j**2] + 1)
+    if is_square(n):
+        print(1)
+        return
+    
+    for i in range(1, int(n**0.5) + 1):
+        if is_square(n - i**2):
+            print(2)
+            return
+    
+    for i in range(1, int(n**0.5) + 1):
+        for j in range(1, int((n - i**2)**0.5) + 1):
+            if is_square(n - i**2 - j**2):
+                print(3)
+                return
             
-    print(dp[-1])
+    print(4)
 
 if __name__ == "__main__":
     solve()
