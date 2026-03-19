11f1fa67390d11782d3a866456a5a9b23d6d28c5
Peter Ryu <101504238+YnRoux@users.noreply.github.com>
2026-03-19 23:20:57 +0900

    [Silver V] Title: 생일, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/5635. 생일/생일.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+def solve():
+    n = int(input())
+    students = []
+    
+    for _ in range(n):
+        name, d, m, y = input().split()
+        students.append([name, int(d), int(m), int(y)])
+
+    students.sort(key=lambda x: (x[3], x[2], x[1]))
+
+    print(students[-1][0])
+    print(students[0][0])
+
+
+if __name__ == "__main__":
+    solve()
