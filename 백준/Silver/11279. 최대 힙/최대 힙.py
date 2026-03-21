180b0956c261290271b75001385a4e89f84ee9cf
Peter Ryu <101504238+YnRoux@users.noreply.github.com>
2026-03-22 07:14:06 +0900

    [Silver II] Title: 최대 힙, Time: 228 ms, Memory: 44748 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.py
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -1,20 +1,59 @@
 import sys
-import heapq
+
+class MaxHeap:
+    def __init__(self):
+        self.heap = [None]
+
+    def push(self, data):
+        self.heap.append(data)
+        idx = len(self.heap) - 1
+        
+        while idx > 1:
+            parent_idx = idx // 2
+            if self.heap[idx] > self.heap[parent_idx]:
+                self.heap[idx], self.heap[parent_idx] = self.heap[parent_idx], self.heap[idx]
+                idx = parent_idx
+            else:
+                break
+
+    def pop(self):
+        if len(self.heap) <= 1:
+            return 0
+        
+        if len(self.heap) == 2:
+            return self.heap.pop()
+
+        root_data = self.heap[1]
+        self.heap[1] = self.heap.pop()
+        
+        idx = 1
+        while idx * 2 < len(self.heap):
+            left_child = idx * 2
+            right_child = idx * 2 + 1
+            largest = left_child
+            
+            if right_child < len(self.heap) and self.heap[right_child] > self.heap[left_child]:
+                largest = right_child
+            
+            if self.heap[idx] < self.heap[largest]:
+                self.heap[idx], self.heap[largest] = self.heap[largest], self.heap[idx]
+                idx = largest
+            else:
+                break
+                
+        return root_data
 
 input = sys.stdin.read
 data = input().split()
 n = int(data[0])
-heap = []
+max_heap = MaxHeap()
 results = []
 
 for i in range(1, n + 1):
     x = int(data[i])
     if x == 0:
-        if not heap:
-            results.append("0")
-        else:
-            results.append(str(-heapq.heappop(heap)))
+        results.append(str(max_heap.pop()))
     else:
-        heapq.heappush(heap, -x)
+        max_heap.push(x)
 
 print("\n".join(results))
