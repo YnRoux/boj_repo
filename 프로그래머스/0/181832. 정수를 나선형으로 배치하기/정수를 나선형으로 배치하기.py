e320119df0b6ae2089f0dc74da80d19312883f1f
Peter Ryu <101504238+YnRoux@users.noreply.github.com>
2026-06-19 10:39:08 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.20 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
@@ -1,14 +1,13 @@
-def solution(n):
+def solution(n):        
     l = [[0] * n for _ in range(n)]
     dr = [0, 1, 0, -1]
     dc = [1, 0, -1, 0]
     r, c = 0, 0
     dir_idx = 0
-    for m in range(1, n**2+1):
+    for m in range(1, n**2 + 1):
         l[r][c] = m
         next_r = r + dr[dir_idx]
         next_c = c + dc[dir_idx]
-        
         if not (0 <= next_r < n and 0 <= next_c < n) or l[next_r][next_c] != 0:
             dir_idx = (dir_idx + 1) % 4
         r += dr[dir_idx]
