3a436d308854777352b0e34d9301411c0cc42f6b
Junha Ryu <101504238+YnRoux@users.noreply.github.com>
2026-08-04 20:49:00 +0900

    [level 0] Title: 2의 영역, Time: 18.68 ms, Memory: 23.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    int first = -1;
+    int last = -1;
+    
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr[i] == 2) {
+            if (first == -1) {
+                first = i;
+            }
+            last = i;
+        }
+    }
+    
+    if (first == -1) {
+        return {-1};
+    }
+    
+    vector<int> answer(arr.begin() + first, arr.begin() + last + 1);
+    
+    return answer;
+}
--- 프로그래머스/0/181894. 2의 영역/README.md
+++ 프로그래머스/0/181894. 2의 영역/README.md
@@ -1,10 +1,10 @@
 # [level 0] 2의 영역 - 181894 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181894) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181894?language=cpp) 
 
 ### 성능 요약
 
-메모리: 13.2 MB, 시간: 0.77 ms
+메모리: 23.5 MB, 시간: 18.68 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 07월 01일 13:30:00
+2026년 08월 04일 20:48:56
 
 ### 문제 설명
 
