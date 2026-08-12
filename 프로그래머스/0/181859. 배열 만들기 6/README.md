be789224898c85a80b90515d3b1e10dc59e7c5c0
Junha Ryu <101504238+YnRoux@users.noreply.github.com>
2026-08-12 11:09:59 +0900

    [level 0] Title: 배열 만들기 6, Time: 5.80 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/0/181859. 배열 만들기 6/README.md
+++ 프로그래머스/0/181859. 배열 만들기 6/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열 만들기 6 - 181859 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181859) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181859?language=cpp) 
 
 ### 성능 요약
 
-메모리: 19.4 MB, 시간: 92.44 ms
+메모리: 33.1 MB, 시간: 5.80 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 07월 04일 13:43:41
+2026년 08월 12일 11:09:56
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    vector<int> stk;
+    for (int i = 0; i < arr.size(); ++i) {
+        if (!stk.empty() && stk.back() == arr[i]) {
+            stk.pop_back();
+        }
+        else {
+            stk.push_back(arr[i]);
+        }
+    }
+    
+    if (stk.empty()) {
+        return {-1};
+    }
+    
+    return stk;
+}
