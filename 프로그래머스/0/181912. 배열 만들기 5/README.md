10fea72ce212c7d727c57ba26da9a8c512a37c21
Junha Ryu <101504238+YnRoux@users.noreply.github.com>
2026-09-06 15:52:55 +0900

    [level 0] Title: 배열 만들기 5, Time: 1.14 ms, Memory: 6.96 MB -BaekjoonHub

--- 프로그래머스/0/181912. 배열 만들기 5/README.md
+++ 프로그래머스/0/181912. 배열 만들기 5/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열 만들기 5 - 181912 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181912?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181912?language=c) 
 
 ### 성능 요약
 
-메모리: 7.05 MB, 시간: 1.92 ms
+메모리: 6.96 MB, 시간: 1.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 07월 31일 14:47:18
+2026년 09월 06일 15:52:51
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+// intStrs_len은 배열 intStrs의 길이입니다.
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+int* solution(const char* intStrs[], size_t intStrs_len, int k, int s, int l) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int* answer = (int*)malloc(sizeof(int) * intStrs_len);
+    int count = 0;
+    
+    char buffer[16];
+    
+    for (size_t i = 0; i < intStrs_len; ++i) {
+        strncpy(buffer, intStrs[i] + s, l);
+        buffer[l] = '\0';
+        
+        int val = atoi(buffer);
+        
+        if (val > k) {
+            answer[count++] = val;
+        }
+    }
+    
+    return answer;
+}
