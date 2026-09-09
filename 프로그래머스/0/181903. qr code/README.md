3171d0bdb4ab52e076cd6499a7ec799dfbca8d1b
Junha Ryu <101504238+YnRoux@users.noreply.github.com>
2026-09-09 11:18:39 +0900

    [level 0] Title: qr code, Time: 0.01 ms, Memory: 4.83 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/README.md
+++ 프로그래머스/0/181903. qr code/README.md
@@ -1,10 +1,10 @@
 # [level 0] qr code - 181903 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181903?language=c) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181903) 
 
 ### 성능 요약
 
-메모리: 5.14 MB, 시간: 0.01 ms
+메모리: 4.83 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 09월 09일 11:12:04
+2026년 09월 09일 11:18:37
 
 ### 문제 설명
 
--- 프로그래머스/0/181903. qr code/qr code.c
+++ 프로그래머스/0/181903. qr code/qr code.c
@@ -8,7 +8,9 @@ char* solution(int q, int r, const char* code) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
     int len = strlen(code);
     
-    char* answer = (char*)malloc(sizeof(char) * (len + 1));
+    int result_len = (len - 1 - r) / q + 1;
+    
+    char* answer = (char*)malloc(sizeof(char) * (result_len + 1));
     if (answer == NULL) {
         return NULL;
     }
